02c672934a5973dcaa27bc3ae17faebf8e34ffde
Kerim Mašović <82513917+KIKI1712@users.noreply.github.com>
2023-09-02 20:30:48 +0200

    Update Approximation_technique_1.R

--- Approximation_technique_1.R
+++ Approximation_technique_1.R
@@ -4,10 +4,9 @@ library(readxl)
 library(tidyverse)
 library(dplyr)
 library(lubridate)
+library(zoo)
+# Load your data (replace 'fake_sales_data.xlsx' with your actual data file)
 sales_data <- read_xlsx("fake_sales_data.xlsx")
-
-#make sure it's date
-class(sales_data$Date)
 
 # Create a binary column indicating stockouts (1 for stockout, 0 for no stockout)
 sales_data$stockout <- ifelse(sales_data$Sales == 0, 1, 0)
@@ -17,33 +16,42 @@ calculate_approximate_demand <- function(data, stockout_index) {
   # Extract the date of the stockout
   stockout_date <- as.Date(data$Date[stockout_index])
   
-  # Calculate the start date (two months before the stockout)
-  start_date <- stockout_date %m-% months(2)
+  # Calculate the start date (15 days before the stockout)
+  start_date <- stockout_date - days(15)
   
   # Calculate the end date (one day before the stockout)
   end_date <- stockout_date - days(1)
   
-  # Filter data for non-stockout days within the last two months, excluding May, June, and July
+  # Filter data for non-stockout days within the last 15 days
   relevant_data <- data %>%
     filter(Date >= start_date & Date <= end_date & stockout == 0)
   
   # Calculate the average sales during these non-stockout days
   average_sales <- mean(relevant_data$Sales)
   
-  # Adjust for seasonality based on the month of the stockout
+  # Adjust for seasonality based on the month and day of the stockout
   stockout_month <- format(stockout_date, "%m")
-  if (stockout_month == "05") {
+  stockout_day <- as.integer(format(stockout_date, "%d"))
+  
+  if (stockout_month == "05" && stockout_day >= 1 && stockout_day <= 10) {
     average_sales <- average_sales * 1.25
-  } else if (stockout_month == "06") {
-    average_sales <- average_sales * 1.45
-  } else if (stockout_month == "07") {
-    average_sales <- average_sales * 1.125
-  } else if (stockout_month == "09") {
+  } else if (stockout_month == "06" && stockout_day >= 1 && stockout_day <= 10) {
+    average_sales <- average_sales * 1.28
+  } else if (stockout_month == "09" && stockout_day >= 1 && stockout_day <= 10) {
     average_sales <- average_sales / 1.28
-  } else if (stockout_month == "10") {
-    average_sales <- average_sales / 1.45
-  } else if (stockout_month == "11") {
-    average_sales <- average_sales / 1.125
+  } else if (stockout_month == "10" && stockout_day >= 1 && stockout_day <= 10) {
+    average_sales <- average_sales / 1.25
+  } else {
+    # Default seasonality adjustments for other cases
+    if (stockout_month == "05") {
+      average_sales <- average_sales * 1.25
+    } else if (stockout_month == "06") {
+      average_sales <- average_sales * 1.28
+    } else if (stockout_month == "09") {
+      average_sales <- average_sales / 1.28
+    } else if (stockout_month == "10") {
+      average_sales <- average_sales / 1.25
+    }
   }
   
   return(average_sales)
